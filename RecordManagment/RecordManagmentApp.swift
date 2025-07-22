20a3f7c1d0a33eb2ca53d31066faf2c21d6da5f5
김용해 <yoho1019@gmail.com>
2025-07-22 21:24:00 +0900

    Initial Commit

---
+++ RecordManagment/RecordManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordManagmentApp.swift
+//  RecordManagment
+//
+//  Created by 김용해 on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecordManagmentTests/RecordManagmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecordManagmentTests.swift
+//  RecordManagmentTests
+//
+//  Created by 김용해 on 7/22/25.
+//
+
+import Testing
+@testable import RecordManagment
+
+struct RecordManagmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
