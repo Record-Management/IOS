cdf95a47d22c14fc243bb4065499baf54fd055d8
김용해 <yoho1019@gmail.com>
2025-07-29 14:04:19 +0900

    ISA-2 feat: 카카오 SDK 초기화 및 로그인/로그아웃 비동기 처리 개선

--- RecordManagment/RecordManagmentApp.swift
+++ RecordManagment/RecordManagmentApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 @main
 struct RecordManagmentApp: App {
+    
+    init() {
+        if let apiKey = Bundle.main.infoDictionary?["KAKAO_API_KEY"] as? String {
+            KakaoSDK.initSDK(appKey: apiKey)
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    _ = AuthController.handleOpenUrl(url: url)
+                }
+            })
         }
     }
 }
--- RecordManagment/ViewModel/KaKaoLoginViewModel.swift
+++ RecordManagment/ViewModel/KaKaoLoginViewModel.swift
@@ -10,24 +10,25 @@ import KakaoSDKUser
 
 protocol LoginService {
     func login() async
-    func lotout() async
+    func logout() async
 }
 
 protocol KaKaoLoginInterface: LoginService {}
 
-class KaKaoLoginService: KaKaoLoginInterface {
+@MainActor
+class KaKaoLoginViewModel: ObservableObject ,KaKaoLoginInterface {
     @Published var token: String? = nil
     
     func login() async {
         if (UserApi.isKakaoTalkLoginAvailable()) {
            await kakaoAppLaunchedLogin()
         } else {
-            kakaoWebViewLogin()
+            await kakaoWebViewLogin()
         }
     }
     
-    func lotout() async {
-        kakaoLogout()
+    func logout() async {
+        await kakaoLogout()
     }
     
     // TODO: 카카오톡이 설치된 경우 Login logic
@@ -48,29 +49,35 @@ class KaKaoLoginService: KaKaoLoginInterface {
     }
     
     // TODO: 카카오톡 설치 안된 경우 -> 웹뷰
-    func kakaoWebViewLogin() {
-        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-            if let error = error {
-                print(error)
-            }
-            else {
-                print("loginWithKakaoAccount() success.")
+    func kakaoWebViewLogin() async {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoAccount() success.")
 
-                // 성공 시 동작 구현
-                self.token = String(describing: oauthToken)
+                    // 성공 시 동작 구현
+                    self.token = String(describing: oauthToken)
+                }
             }
+            continuation.resume()
         }
     }
     
     // TODO: 카카오 로그아웃
-    func kakaoLogout() {
-        UserApi.shared.logout {(error) in
-            if let error = error {
-                print(error)
+    func kakaoLogout() async {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.logout {(error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("logout() success.")
+                }
             }
-            else {
-                print("logout() success.")
-            }
+            continuation.resume()
         }
     }
 }
