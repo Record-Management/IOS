9ba731b062c0bec3a84cde34f0e65b57c79ebf26
김용해 <yoho1019@gmail.com>
2025-07-29 14:01:33 +0900

    ISA-2 feat: 카카오 로그인 서비스 구현

---
+++ RecordManagment/ViewModel/KaKaoLoginViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  KaKaoLoginService.swift
+//  RecordManagment
+//
+//  Created by 김용해 on 7/29/25.
+//
+
+import Foundation
+import KakaoSDKUser
+
+protocol LoginService {
+    func login() async
+    func lotout() async
+}
+
+protocol KaKaoLoginInterface: LoginService {}
+
+class KaKaoLoginService: KaKaoLoginInterface {
+    @Published var token: String? = nil
+    
+    func login() async {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+           await kakaoAppLaunchedLogin()
+        } else {
+            kakaoWebViewLogin()
+        }
+    }
+    
+    func lotout() async {
+        kakaoLogout()
+    }
+    
+    // TODO: 카카오톡이 설치된 경우 Login logic
+    func kakaoAppLaunchedLogin() async {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    // 성공 시 동작 구현
+                    self.token = String(describing: oauthToken)
+                }
+            }
+            continuation.resume()
+        }
+    }
+    
+    // TODO: 카카오톡 설치 안된 경우 -> 웹뷰
+    func kakaoWebViewLogin() {
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+
+                // 성공 시 동작 구현
+                self.token = String(describing: oauthToken)
+            }
+        }
+    }
+    
+    // TODO: 카카오 로그아웃
+    func kakaoLogout() {
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("logout() success.")
+            }
+        }
+    }
+}
