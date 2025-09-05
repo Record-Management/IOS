7ec633df2a5a45d53e1c723050b1c3a745952df2
김용해 <yoho1019@gmail.com>
2025-09-05 18:30:36 +0900

    feat ISA-5: 알림 권한 확인 함수 작성

--- RecordManagment/UseCases/NotificationService.swift
+++ RecordManagment/UseCases/NotificationService.swift
@@ -10,6 +10,7 @@ import UserNotifications
 class NotificationService {
     let center = UNUserNotificationCenter.current()
     
+    // 알림 권한 요청
     func requestNotificationPermission() async -> Bool {
         await withCheckedContinuation { continuation in
             center.requestAuthorization(options: [.alert, .badge, .sound]) { grant, err in
@@ -17,6 +18,20 @@ class NotificationService {
                     debugPrint("알림 권한 요청 실패 : \(err.localizedDescription)")
                 }
                 continuation.resume(returning: grant)
+            }
+        }
+    }
+    
+    // 알림 권한 확인
+    func checkNotificationAuthorizationStatus() async -> Bool {
+        await withCheckedContinuation { continuation in
+            center.getNotificationSettings { settings in
+                switch settings.authorizationStatus {
+                case .authorized, .provisional, .ephemeral:
+                    continuation.resume(returning: true)
+                default:
+                    continuation.resume(returning: false)
+                }
             }
         }
     }
