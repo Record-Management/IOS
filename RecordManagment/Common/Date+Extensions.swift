4155892a85d20f8d013409b87f932fba61cba185
김용해 <yoho1019@gmail.com>
2025-09-17 03:21:12 +0900

    feat ISA-10: 날짜 형식 변환 기능 추가 및 한국어 로케일 설정

--- RecordManagment/Common/Date+Extensions.swift
+++ RecordManagment/Common/Date+Extensions.swift
@@ -12,7 +12,23 @@ extension Date {
     // OnBoarding Convert func for Reqeust Body
     static func onBoardingFormet(_ date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-06-02"
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter.string(from: date)
+    }
+    
+    // Daily Record Format ex) 2025.09.15 (월)
+    static func dailyRecordDateFormat(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd (E)"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter.string(from: date)
+    }
+    
+    // Daily Time Record Format ex) 오전 02:32
+    static func dailyTimeRecordDateFormat(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "a hh:mm"
         dateFormatter.locale = Locale(identifier: "ko_KR")
         return dateFormatter.string(from: date)
     }
