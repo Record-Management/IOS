042b4ec8878c4758d19d81ba9b8196b817fb08f2
김용해 <yoho1019@gmail.com>
2025-09-18 17:27:44 +0900

    feat ISA-10: Calender DTO 생성

---
+++ RecordManagment/Model/CalenderRecord.swift
@@ -0,0 +1,33 @@
+//
+//  CalenderRecord.swift
+//  RecordManagment
+//
+//  Created by 김용해 on 9/17/25.
+//
+
+import Foundation
+
+struct CalenderRecord: Codable {
+    let statusCode: Int
+    let code: String
+    let message: String
+    let data: CalenderData?
+}
+
+
+struct CalenderData: Codable {
+    let year: Int
+    let month: Int
+    let dailyRecords: [AllRecord]?
+}
+
+struct AllRecord: Codable {
+    let date: [Int]
+    let records: [DetailRecord]
+}
+
+struct DetailRecord: Codable {
+    let id: String
+    let type: String
+    let emotion: String
+}
