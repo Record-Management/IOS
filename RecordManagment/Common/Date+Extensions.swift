f1803a6b622888c7ebd34b90979df41a07bb2797
김용해 <yoho1019@gmail.com>
2025-09-23 01:49:59 +0900

    REFACTOR ISA-10: 서버 date 코드 형식 변환으로 인해 Date 형변환 코드 작성

--- RecordManagment/Common/Date+Extensions.swift
+++ RecordManagment/Common/Date+Extensions.swift
@@ -37,10 +37,14 @@ extension Date {
     static func dailyTimeRecordDateFormat(_ date: [Int]) -> String {
         guard date.count == 2 else { return "" }
         let component = DateComponents(hour: date[0], minute: date[1])
+        let calender = Calendar.current
         let dateFormatter = DateFormatter()
+        
         dateFormatter.dateFormat = "a hh:mm"
         dateFormatter.locale = Locale(identifier: "ko_KR")
-        return dateFormatter.string(from: component.date ?? .now)
+        
+        let componentDate = calender.date(from: component)
+        return dateFormatter.string(from: componentDate ?? .now)
     }
     
     // dateformat만 바꿔서 사용가능한 DateFormat 함수
@@ -49,5 +53,12 @@ extension Date {
         dateFormatter.dateFormat = format
         dateFormatter.locale = Locale(identifier: "ko_KR")
         return dateFormatter.string(from: date)
+    }
+    
+    static func convertDateForIntArray(_ arr: [Int]) -> Date? {
+        guard arr.count == 3 else { return nil }
+        let component = DateComponents(year: arr[0], month: arr[1], day: arr[2])
+        
+        return Calendar.current.date(from: component)
     }
 }
