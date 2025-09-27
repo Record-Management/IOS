4d861ac218eb6f3b968b3270e8d5a327b04de167
김용해 <yoho1019@gmail.com>
2025-09-27 15:32:29 +0900

    feat ISA-10: 감정 문자열을 enum 값으로 변환하는 기능 추가 및 EmotionView를 전체 화면 분기

--- RecordManagment/Model/EmotionObj.swift
+++ RecordManagment/Model/EmotionObj.swift
@@ -24,4 +24,30 @@ enum EmotionObj: String, CaseIterable {
         
         return fWord + restWord
     }
+    
+    // 서버 Emotion String 값을 enum 값에 맞게 변환
+    static func matchingEmotion(_ emotion: String) -> EmotionObj {
+        switch emotion {
+            case "Normal":
+                return .normal
+            case "Happy":
+                return .happy
+            case "Peaceful":
+                return .peaceful
+            case "Funny":
+                return .funny
+            case "Love":
+                return .love
+            case "Tired":
+                return .tired
+            case "Panic":
+                return .panic
+            case "Sad":
+                return .sad
+            case "Angry":
+                return .angry
+            default:
+                return .normal
+        }
+    }
 }
