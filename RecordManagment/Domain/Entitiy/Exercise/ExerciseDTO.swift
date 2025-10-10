f12f0dc3b8d40e82a8b11b0c642b2d76466b85fb
김용해 <yoho1019@gmail.com>
2025-10-10 19:17:10 +0900

    REFACTOR : 회의 내용 적용, 수정 시 전체 Parameter 전달 -> 값이 없음 또한 수정이 될수 있다

--- RecordManagment/Domain/Entitiy/Exercise/ExerciseDTO.swift
+++ RecordManagment/Domain/Entitiy/Exercise/ExerciseDTO.swift
@@ -2,13 +2,13 @@ import Foundation
 
 struct ExerciseBody: Codable {
     let exerciseType: String
-    let caloriesBurned: Int?
-    let exerciseTimeMinutes: Int?
-    let stepCount: Int?
-    let weight: Int?
+    let caloriesBurned: Int
+    let exerciseTimeMinutes: Int
+    let stepCount: Int
+    let weight: Int
     let dailyNote: String
     var imageUrls: [String]
-    let recordDate: String
+    let recordDate: String?
     let recordTime: String
 }
 
