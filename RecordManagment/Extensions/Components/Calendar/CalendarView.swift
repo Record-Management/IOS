160a39bb46bbb6eec036c26f7286ad80b2745230
김용해 <yoho1019@gmail.com>
2025-10-12 16:36:17 +0900

    feat ISA-17: calendar datePicker wheel style 및 달력 이동 구현

--- RecordManagment/Extensions/Components/Calendar/CalendarView.swift
+++ RecordManagment/Extensions/Components/Calendar/CalendarView.swift
@@ -1,9 +1,17 @@
 import SwiftUI
 
+
+struct MiddleSizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
+
 struct CalendarView: View {
     @ObservedObject var vm: ViewModel // Calendar ViewModel
     @EnvironmentObject var sheetVM: MainSheetViewModel
-    
+    @State private var datePickerSize: CGSize = .zero
     let columns = Array(repeating: GridItem(.flexible(), spacing: 0), count: 7)
     var dragProgress: CGFloat = 1
     
@@ -12,19 +20,43 @@ struct CalendarView: View {
             headerView
                 .padding(.bottom, 10)
                 .zIndex(1)
-            middleDays
-            
-            MonthCalendarView(
-                isDragging: false,
-                dragProgress: dragProgress,
-                title: $vm.title,
-                focused: $vm.focusedWeek,
-                selection: $vm.date,
-                currentRecord: $vm.currentRecord,
-                calendarRecord: $vm.calendarRecord,
-                selectedMonth: $vm.selectedMonth
-            )
-            .frame(maxHeight: Calendar.monthHeight)
+            Group {
+                if vm.dateMode {
+                    DatePicker(
+                        "", // 라벨 텍스트를 빈 문자열로
+                        selection: $vm.selectedMonth,
+                        displayedComponents: [.date] // 날짜만 선택
+                    )
+                    .datePickerStyle(.wheel)  // Wheel 스타일
+                    .labelsHidden()           // 라벨 숨김
+                    .font(.system(size: 28, weight: .bold))
+                    .frame(maxWidth: .infinity, maxHeight: datePickerSize.height + Calendar.monthHeight)
+                    .environment(\.locale, Locale(identifier: "ko_KR"))
+                    .scaleEffect(1.3)
+                    .clipped()
+                    .onChange(of: vm.selectedMonth) {
+                        vm.title = Calendar.monthAndYear(from: vm.selectedMonth)
+                        vm.date = vm.selectedMonth
+                    }
+                } else {
+                    middleDays
+                        .onPreferenceChange(MiddleSizePreferenceKey.self) { newSize in
+                            datePickerSize = newSize
+                        }
+                    
+                    MonthCalendarView(
+                        isDragging: false,
+                        dragProgress: dragProgress,
+                        title: $vm.title,
+                        focused: $vm.focusedWeek,
+                        selection: $vm.date,
+                        currentRecord: $vm.currentRecord,
+                        calendarRecord: $vm.calendarRecord,
+                        selectedMonth: $vm.selectedMonth
+                    )
+                    .frame(maxHeight: Calendar.monthHeight)
+                }
+            }
             .onChange(of: sheetVM.visibleToast) {
                 if sheetVM.visibleToast { // Toast가 활성화 되면 캘린더 업데이트
                     vm.currentRecord = vm.currentRecord
@@ -38,10 +70,18 @@ struct CalendarView: View {
     // TODO: 상단 현재 year, month 및 색상 뷰
     private var headerView: some View {
         HStack {
-            Text(vm.title)
-                .typography(.p20Bold)
-                .lineLimit(1)
-                .minimumScaleFactor(0.5)
+            HStack {
+                Text(vm.title)
+                    .typography(.p20Bold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                Image(systemName: "chevron.down")
+            }
+            .onTapGesture {
+                withAnimation {
+                    vm.dateMode.toggle() // active Date Mode
+                }
+            }
             Spacer()
             ZStack {
                 Rectangle()
@@ -90,6 +130,11 @@ struct CalendarView: View {
                     .padding(.vertical, 9)
                     .frame(maxWidth: .infinity)
             }
+            .background(
+                GeometryReader { geo in
+                    Color.clear.preference(key: MiddleSizePreferenceKey.self, value: geo.size)
+                }
+            )
         }
     }
 }
@@ -112,4 +157,5 @@ extension CalendarView {
             )
         )
     )
+    .environmentObject(MainSheetViewModel())
 }
