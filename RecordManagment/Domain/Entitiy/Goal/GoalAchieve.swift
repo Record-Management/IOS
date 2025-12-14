40e1c3821e4b51c9f1b48254ca5112ad03c5ab69
김용해 <yoho1019@gmail.com>
2025-12-14 17:05:05 +0900

    REFACTOR QA-19: 잘못된 모델링 수정

--- RecordManagment/Domain/Entitiy/Goal/GoalAchieve.swift
+++ RecordManagment/Domain/Entitiy/Goal/GoalAchieve.swift
@@ -1,23 +1,38 @@
 import Foundation
 
 struct GoalAchieve: Decodable {
+    let statusCode: Int
+    let code: String
+    let message: String
     let data: GoalData?
-    let achieveCount: Int?
-    
-    enum CodingKeys: String, CodingKey {
-        case data = "currentPeriod"
-        case achieveCount = "cumulativeAchievementCount"
-    }
 }
 
 struct GoalData: Decodable {
+    let currentPeriod: CurrentPeriodData?
+    let cumulativeAchievementCount: Int
+    let recentHistory: [RecentHistoryData?]
+}
+
+struct CurrentPeriodData: Decodable {
     let goalId: String
     let recordType: String
     let goalDays: Int
-    let startDate: String
-    let endDate: String
+    let startDate: [Int]
+    let endDate: [Int]
     let completedDays: Int
     let achievementRate: Int
     let treeStage: String
-    let isInProgress: Bool
+    let isInProgress: Bool?
+}
+
+struct RecentHistoryData: Decodable {
+    let goalId: String
+    let recordType: String
+    let goalDays: Int
+    let startDate: [Int]
+    let endDate: [Int]
+    let completedDays: Int
+    let achievementRate: Int
+    let finalTreeStage: String
+    let status: String
 }
