63d1eb43704deada55d6d27124d317d47efe66ce
김용해 <yoho1019@gmail.com>
2026-02-12 22:55:16 +0900

    FEAT: Close 및 Loader 이미지셋 추가 및 LoaderView 구현

---
+++ RecordManagment/Presentation/View/Main/LoaderView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct LoaderView: View {
+    @State private var navHeight: CGFloat = .zero
+    @Binding var isShow: Bool
+
+    init(isShow: Binding<Bool>) {
+        self._isShow = isShow
+    }
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            let size: CGSize = geo.size
+            ZStack {
+                Color.white.ignoresSafeArea()
+                
+                NavigationBarProxy { _, bar, _ in
+                    navHeight = bar.bounds.height
+                }
+                
+                Image("Loader")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .position(x: size.width / 2, y: size.height * 0.45 - navHeight)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Image("Notification")
+                    .higTouchArea()
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Image("Setting")
+                    .higTouchArea()
+            }
+        }
+        .task {
+            try? await Task.sleep(for: .seconds(0.3))
+            isShow = false
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        LoaderView(
+            isShow: .constant(false)
+        )
+    }
+}
