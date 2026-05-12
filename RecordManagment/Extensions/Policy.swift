425e74cd0cd198a17b00eb1d76bd99f885a60674
Kimyonhae <yoho1019@naver.com>
2026-05-13 02:46:06 +0900

    feat(domain): 매직 넘버 정리

--- RecordManagment/Extensions/Policy.swift
+++ RecordManagment/Extensions/Policy.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class Policy {
+final class Policy {
     /// 앱 리뷰 URL 주소
     static let AppReViewURL: String = "https://apps.apple.com/kr/app/%EC%94%A8%EB%93%9C-%EB%8D%B0%EC%9D%B4/id6753913555"
     /// 씨드데이 정책 URL 주소
